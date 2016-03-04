c2c87c2c65a566dfd7f20dc8ed6a1083e760d6e3
robcodonnell <robodonnell@Robs-MacBook-Pro.local>
2016-03-04 16:57:56 -0500

    wrote the function for berries

--- js/main.js
+++ js/main.js
@@ -36,6 +36,39 @@ var redCurrant = {
 
 // Write your function here, name it `writeBerries`
 
+var writeBerries = function (berries) {
+  document.write('<h1>Berries</h1>');
+  document.write('<ul>');
+
+  berries.forEach(function (berry) {
+    document.write('<li>');
+    document.write('<h2>' + berry.name + '</h2>');
+    document.write('<dl>');
+    document.write('<dt>Grows on a shrub?</dt>');
+    document.write('<dd>');
+
+    if (berry.onShrub) {
+      document.write('Yes');
+    }  else {
+      document.write('No');
+    }
+
+    document.write('</dd>');
+
+    document.write('<dt>Energy</dt>');
+    document.write('<dd>' + berry.energy + ' kJ</dd>');
+    document.write('<dt>Carbohydrates</dt>');
+    document.write('<dd>' + berry.carbs + ' g</dd>');
+    document.write('<dt>Protein</dt>');
+    document.write('<dd>' + berry.protein + ' g</dd>');
+
+    document.write('</dl>');
+
+    document.write('</li>');
+
+    document.write('</ul>');
+  });
+};
 
 // ##########################################################
 // START DO NOT CHANGE ZONE
